01947a67e741d07e7cb963bc93279ab58965efc8
Gonzalo Robles <gonzalorobles1984@gmail.com>
2023-10-10 19:35:54 -0300

    correccion del boton descargar

--- src/app/modules/generate-cv/generate-cv/generate-cv.component.ts
+++ src/app/modules/generate-cv/generate-cv/generate-cv.component.ts
@@ -21,7 +21,8 @@ import jsPDF from "jspdf";
   styleUrls: ['./generate-cv.component.css']
 })
 export class GenerateCvComponent implements OnInit {
-  
+  @ViewChild('impresionPDF')
+  impresionPDF!: ElementRef<HTMLElement>;
   id!: string;
   datosCv: any [] = [];
   token:string = "";
@@ -114,5 +115,41 @@ export class GenerateCvComponent implements OnInit {
   setValue(arg0: { name: any; lastName: any; address: any; email: any; phone: any; }) {
     throw new Error('Method not implemented.');
   }
-
+  exportPDF() {
+    
+    
+    if (this.impresionPDF) {
+      html2canvas(this.impresionPDF.nativeElement, { scale: 2 }).then((canvas) => {
+        // Aumenta la escala para mejorar la calidad
+        const imgData = canvas.toDataURL('image/jpeg');
+        const pdf = new jsPDF({
+          orientation: 'portrait',
+          unit: 'mm',
+          format: 'a4'
+        });
+  
+        const imageProps = pdf.getImageProperties(imgData);
+        const pdfWidth = 210; // Ancho en milímetros para A4
+        const pdfHeight = 297; // Altura en milímetros para A4
+  
+        // Calcula el ancho y alto ajustados para la imagen en el PDF
+        const aspectRatio = imageProps.width / imageProps.height;
+        let imgWidth = pdfWidth;
+        let imgHeight = pdfWidth / aspectRatio;
+  
+        if (imgHeight > pdfHeight) {
+          imgHeight = pdfHeight;
+          imgWidth = pdfHeight * aspectRatio;
+        }
+  
+        // Posiciona la imagen a 20px desde la parte superior del PDF
+        const x = (pdfWidth - imgWidth) / 2;
+        const y = 20;
+  
+        pdf.addImage(imgData, 'JPEG', x, y, imgWidth, imgHeight);
+  
+        pdf.save('CV-Generado.pdf');
+      });
+    }
+  }
 }
