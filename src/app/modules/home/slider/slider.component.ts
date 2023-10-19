5f0cf20e5d668abc4833e264d5af369b724e921b
Gonzalo Robles <gonzalorobles1984@gmail.com>
2023-10-19 10:48:15 -0300

    Se automatizo el slide

--- src/app/modules/home/slider/slider.component.ts
+++ src/app/modules/home/slider/slider.component.ts
@@ -6,14 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent implements OnInit {
-  slide:number = 1;
-  
-  constructor() { }
+  slide: number = 1; // Índice del slide actual
+  totalSlides: number = 3; // Número total de slides
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit() {
+    // Iniciar la automatización del slider
+    this.startAutoSlide();
   }
 
+  // Función para actualizar el slide
   updateSlide(newSlide: number) {
     this.slide = newSlide;
+  }
+
+  // Función para cambiar automáticamente el slide
+  startAutoSlide() {
+    setInterval(() => {
+      this.slide = (this.slide % this.totalSlides) + 1;
+    }, 3000); // Cambia de slide cada 3 segundos (ajusta el intervalo según tus preferencias)
   }
 }
